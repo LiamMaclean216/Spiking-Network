8f4d343448c1dcd9b47b21f8b0672bfb1c4fae68
Liam Maclean <lmaclean234@gmail.com>
2020-01-20 13:28:07 -0500

    Neuron-centric network

--- utils.py
+++ utils.py
@@ -1,51 +1,45 @@
 import torch
+import math
 
-#A block of every other spiking layer (updates them all at once)
-class Spike1d_block(torch.nn.Module):
-    def __init__(self, layers):
-        super(Spike1d_block, self).__init__()
-        self.layers = layers
+def sig(x):
+    return (1 / (1 + torch.exp(-x)))
+
+def sig_d(x):
+    s = sig(x)
+    return s * (1 - s)
+
+
+
+class Dense(torch.nn.Module):
+    def __init__(self, n_in, n_out , sigmoid = False, pointwise = False):
+        super(Dense, self).__init__()
+        self.n_in = n_in
+        self.n_out = n_out
+        self.sigmoid = sigmoid
+        self.pointwise = pointwise
         
-    def forward(self,in_spikes , training = True):
-        out_spikes = []
-        out_pots = []
-        for idx,l in enumerate(self.layers):
-            o, p = l(in_spikes[idx:idx + l.get_n_inputs()], training)
-            out_pots.append(p)
-            out_spikes.append(o)
-        
-        return out_spikes, out_pots
+        self.weight = torch.ones([n_in, n_out])
+            
     
-    def get_empty_spikes(self):
-        out = []
-        for i in self.layers:
-            out.append(i.get_empty_spikes())
-        return out
+       # self.weight += -1
+        self.reset_parameters()
 
-#this function recombines and draws the lists of odd and even spikes
-def draw_spikes(spikes, ax, l_dim = 4, n_layers = 3):
-    to_draw = torch.zeros(n_layers,l_dim) - 0.1
-    e = even = odd = 0
-    while True:
-        e += 1
-        if e % 2 != 0:
-            spike_block = spikes[0]
-            i = odd
-            odd += 1
-        else:
-            spike_block = spikes[1]
-            i = even
-            even += 1
+    def forward(self, spikes):
+        a = torch.zeros([self.n_out])
+        for r, c in zip(spikes,self.weight):
+            if self.sigmoid:
+                a += sig(r * c) * r
+            else:
+                a += (r * c)
         
-        if(i > len(spike_block)-1):
-            break
-        offset = (l_dim - spike_block[i].shape[0]) // 2
-        try:
-            to_draw[e-1][offset:spike_block[i].shape[0] + offset] = spike_block[i].clone().detach()
-        except:
-            to_draw[e-1][offset:spike_block[i].shape[0] + offset] = torch.tensor(spike_block[i])
+        #print(self.weight)
+        return a
 
-    to_draw = to_draw.t()
-    
-    ax.clear()
-    ax.imshow(to_draw,vmin=-0.1, vmax=1)
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.weight.size(0))
+        self.weight.data.uniform_(0, stdv)
+        
+    def weight_update(self, error, lr = 1):
+        delta_w = sig_d(self.weight) * sig_d(error/5) * torch.sign(error) * lr
+        self.weight += delta_w    
+        return (sig(self.weight))
